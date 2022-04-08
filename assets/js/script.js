47fe71db3f1eabad7b59579ff0edf2a6033c1368
Cecilia Glynn <ceciliaglynn@icloud.com>
2022-04-08 15:30:45 -0700

    Hides start button when game begins & starts timer

---
+++ assets/js/script.js
@@ -0,0 +1,100 @@
+// The array listOfQuestions is comprised of question objects
+const listOfQuestions =[
+    {
+    questionNumber: 0,
+    question: "Commonly used data types DO NOT include:",
+    answers: {
+        a: "Strings",
+        b: "Booleans",
+        c: "Alerts",
+        d: "Numbers"
+    },
+    correctAns: "c"
+},{
+    questionNumber: 1,
+    question: "The condition in an if/else statement is enclosed within_____.",
+    answers: {
+        a: "Quotes",
+        b: "Curly Brackets",
+        c: "Parentheses",
+        d: "Square Brackets"
+    },
+    correctAns: "c"
+},{
+    questionNumber: 2,
+    question: "Arrays in JavaScript can be used to store:",
+    answers: {
+        a: "Numbers & Strings",
+        b: "Other Arrays",
+        c: "Booleans",
+        d: "All of the above"
+    },
+    correctAns: "d"
+},{
+    questionNumber: 3,
+    question: "String values must be enclosed within _____ when being assigned into variables.",
+    answers: {
+        a: "Commas",
+        b: "Curly Brackets",
+        c: "Quotes",
+        d: "Parentheses"
+    },
+    correctAns: "c"
+},{
+    questionNumber: 4,
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: {
+        a: "JavaScript",
+        b: "Terminal/Bash",
+        c: "For Loops",
+        d: "console.log"
+    },
+    correctAns: "d"
+}
+];
+
+
+
+// Start the game
+/*  WHEN WE START THE GAME:
+1. Initialize all variables and scores
+2. Hide startButton when start is clicked
+3. Begin timer
+*/  
+    // Will this return any values?
+    const startButton = document.querySelector("#start");
+    startButton.addEventListener("click", beginGame, setGameTimer);
+    const displayQuestion = document.querySelector("#quizQuestion");
+    const timeElement = document.querySelector("#time");
+    
+    
+    function beginGame(){
+        startButton.classList.add("hide");
+        setGameTimer();
+    }
+
+    // This function sets the initial time for the game 
+    var secondsLeft = 75;
+    function setGameTimer() {
+        // Sets interval in variable
+        var timerInterval = setInterval(function() {
+          secondsLeft--;
+          timeElement.textContent = secondsLeft + " seconds left.";
+      
+          if(secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+          }
+        }, 1000);
+      }
+
+
+    /*  WHEN USER CLICKS ON AN ANSWER
+        1. Add or subtract score
+        2. Add or subtract time
+        3. Show the next card */
+// Populate the question and answers
+// Subtract time if answer is wrong
+// Calculate score
+
+
