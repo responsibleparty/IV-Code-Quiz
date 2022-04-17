e3b89a1cb6ed3b185799be08054191314fd6ee26
Cecilia Glynn <ceciliaglynn@icloud.com>
2022-04-16 20:13:52 -0700

    Updated Game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,125 +1,240 @@
-// Links HTML startButton to the variable
-var startButton = document.querySelector("#startButton"); // Links HTML startButton to the variable
-    startButton.addEventListener("click", startGame); // Adds event listener, onclick will call startGame function
-var timeElement = document.querySelector("#time");
+var startButton = document.querySelector("#startButton");
+startButton.addEventListener("click", beginGame);
+var timeArea = document.querySelector("#time");
 var instructions = document.querySelector("#instructions");
-// Linking quizSection
-var quizQuestion = document.querySelector("#quizQuestion");
-var quizSection = document.querySelector("#quizSection");
-var option1 = document.querySelector(".option1");
-var option2 = document.querySelector(".option2");
-var option3 = document.querySelector(".option3");
-var option4 = document.querySelector(".option4");
+var viewHighScoreButton = document.querySelector("#viewHighScoreButton");
+var quizContent = document.querySelector("#quizContent");
+var clearHighScoresButton = document.querySelector(".clearHighScoresButton");
 
-// Array of question OBJECTS
-const listOfQuestions = [
-    {
-      questionNumber: 0,
-      question: "Commonly used data types DO NOT include:",
-      answers: {
-        a: "Strings",
-        b: "Booleans",
-        c: "Alerts",
-        d: "Numbers",
-      },
-      correctAns: "Alerts",
-    },
-    {
-      questionNumber: 1,
-      question: "The condition in an if/else statement is enclosed within_____.",
-      answers: {
-        a: "Quotes",
-        b: "Curly Brackets",
-        c: "Parentheses",
-        d: "Square Brackets",
-      },
-      correctAns: "Parentheses",
-    },
-    {
-      questionNumber: 2,
-      question: "Arrays in JavaScript can be used to store:",
-      answers: {
-        a: "Numbers & Strings",
-        b: "Other Arrays",
-        c: "Booleans",
-        d: "All of the above",
-      },
-      correctAns: "All of the above",
-    },
-    {
-      questionNumber: 3,
-      question:
-        "String values must be enclosed within _____ when being assigned into variables.",
-      answers: {
-        a: "Commas",
-        b: "Curly Brackets",
-        c: "Quotes",
-        d: "Parentheses",
-      },
-      correctAns: "Quotes",
-    },
-    {
-      questionNumber: 4,
-      question:
-        "A very useful tool used during development and debugging for printing content to the debugger is:",
-      answers: {
-        a: "JavaScript",
-        b: "Terminal/Bash",
-        c: "For Loops",
-        d: "console.log",
-      },
-      correctAns: "console.log",
-    },
-  ];
+var stopTimer = false;
+let finalScore = 0;
+var timer = 0;
+var questionsQueue = [];
+var currentQuestion = {};
+const questionBank = [
+  {
+    questionNumber: 0,
+    question: "What is my favorite movie?",
+    answers: ["Napoleon Dynamite", "Big Fish", "In Bruges", "Resident Evil"],
+    correctAns: "Napoleon Dynamite",
+  },
+  {
+    questionNumber: 1,
+    question: "Who is my favorite dog?",
+    answers: ["Grandpa", "Husky", "Gibby & Mila", "All the above"],
+    correctAns: "All the above",
+  },
+  {
+    questionNumber: 2,
+    question: "What is my favorite hobby?",
+    answers: ["Knitting", "Baking", "Crocheting", "Sleeping"],
+    correctAns: "Crocheting",
+  },
+  {
+    questionNumber: 3,
+    question: "What time do i wake up?",
+    answers: ["7 AM", "8 AM", "9 AM", "Noon"],
+    correctAns: "7 AM",
+  },
+  {
+    questionNumber: 4,
+    question: "What is my favorite cereal?",
+    answers: [
+      "Crunch Berries",
+      "Oops, All Berries!",
+      "Golden Grahams",
+      "Cinnamon Toast Crunch",
+    ],
+    correctAns: "Oops, All Berries!",
+  },
+  {
+    questionNumber: 5,
+    question: "What time is it?",
+    answers: [
+      "Time to take a nap",
+      "Time to wake up",
+      "It's Modelo Time foo",
+      "Time to crochet!",
+    ],
+    correctAns: "It's Modelo Time foo",
+  },
+];
 
-  // Initialize global variables
-  let totalScore = 0;
-  let arrayIndex=0;
-  let secondsLeft = 75;
+function beginGame() {
+  stopTimer = false;
+  finalScore = 0;
+  timer = 0;
+  questionsQueue = [];
+  currentQuestion = {};
+  hideButtons();
+  setGameTimer();
+  for (let question of questionBank) {
+    questionsQueue.push(question);
+  }
+  currentQuestion = questionsQueue[0];
+  setGameBoard();
+}
 
-  function startGame(){
-    quizSection.style.display = "block";
-    instructions.style.display = "none";
-    startButton.style.display = "none";
-    setGameTimer();
-    populateGameBoard();
+function setGameBoard() {
+  // pull from questions array - display question
+  // pull answers from same array + append each answer
+  let answersOfThisQuestion = currentQuestion.answers;
+  answersOfThisQuestion = shuffleAnswers(answersOfThisQuestion);
+  console.log(answersOfThisQuestion);
+
+  let htmlTemplate = `
+    <h2>${currentQuestion.question}</h2>
+        <button data-content="${answersOfThisQuestion[0]}" class="answerButton">A. ${answersOfThisQuestion[0]}</button>
+        <button data-content="${answersOfThisQuestion[1]}" class="answerButton">B. ${answersOfThisQuestion[1]}</button>
+        <button data-content="${answersOfThisQuestion[2]}" class="answerButton">C. ${answersOfThisQuestion[2]}</button>
+        <button data-content="${answersOfThisQuestion[3]}" class="answerButton">D. ${answersOfThisQuestion[3]}</button>
+
+    `;
+  quizContent.innerHTML = htmlTemplate;
+}
+
+quizContent.addEventListener("click", function (event) {
+  let element = event.target;
+  // Controls/Listens to answerButton when the current question displays
+  if (element.matches(".answerButton")) {
+    console.log(element.dataset.content);
+    // check if user clicks on wrong or right answer
+    // if user answers right:
+    if (checkAnswer(element.dataset.content)) {
+      console.log("You are correct");
+    } else {
+      console.log("Wrong answer");
+
+      timer = timer - 15;
+    }
+    showNextQuestion();
+  }
+  // When the game is over already - The view will display the form that prompts user to input their name
+  // This deals with the user form
+  if (element.matches(".submitName")) {
+    var userName = document.querySelector("#userName").value;
+    console.log(userName);
+    // Let user userRecord be an array of two values: userName & finalScore
+    let userRecord = [userName, finalScore];
+    // Checks if there is a "key"/data of "Records" then it will return that data
+    // If NOT, it will return an empty array
+    // "Records" is a key value, the array is the value
+    let localRecords = JSON.parse(localStorage.getItem("Records")) || [];
+    // Push the new player into the local Records array
+    localRecords.push(userRecord);
+    // Store it back into local storage
+    localStorage.setItem("Records", JSON.stringify(localRecords));
+
+    displayAllUserHighScores();
   }
 
-  function populateGameBoard(){
-      quizQuestion.innerHTML = listOfQuestions[arrayIndex].question;
-      option1.innerHTML = listOfQuestions[arrayIndex].answers.a;
-      option2.innerHTML = listOfQuestions[arrayIndex].answers.b;
-      option3.innerHTML = listOfQuestions[arrayIndex].answers.c;
-      option4.innerHTML = listOfQuestions[arrayIndex].answers.d;
-
-      return questionNumber;
+  if (element.matches(".goBackButton")) {
+    // quizContent.innerHTML = "";
+    unhideButtons();
+    // Clear the view & unhide buttons
+    // Reset all global variables, queue, and questions
+    restartEverything();
   }
 
-  option1.addEventListener("click", showNextQuestion);
-  option2.addEventListener("click", showNextQuestion);
-  option3.addEventListener("click", showNextQuestion);
-  option4.addEventListener("click", showNextQuestion);
+  if ((element.matches(".clearHighScoresButton"))) {
+    localStorage.setItem("Records", JSON.stringify([]));
+    quizContent.innerHTML = "";
+    startButton.style.display = "block";
+    viewHighScoreButton.style.display = "block";
 
-  // Updates the arrayIndex
-  function showNextQuestion(){
-      let usersPick = this.textContent // Retrieves the letter picked by the user
-      if (usersPick === listOfQuestions[arrayIndex].correctAns){
-          currentScore = secondsLeft;
-      } else{
-          secondsLeft = secondsLeft - 15;
-          currentScore = secondsLeft;
-      }
-      arrayIndex++;
-      populateGameBoard()
   }
+});
 
-  function setGameTimer(){
-      let timeInterval = setInterval(function(){
-          secondsLeft--;
-          timeElement.textContent = secondsLeft + " seconds left!";
-          if (secondsLeft === 0){
-              // Stops execution of action at set interval
-              clearInterval(timeInterval);
-          }
-      }, 1000);
+// Looks inside the Records key, retrieves player name and scores
+function displayAllUserHighScores() {
+  let getHighScoresBack = JSON.parse(localStorage.getItem("Records")) || [];
+  // compare the score - score is the 2nd value of user array
+  // The structure returned is an array within an array
+  getHighScoresBack.sort(function (a, b) {
+    return b[1] - a[1];
+  });
+  // Gets array back
+  console.log(getHighScoresBack);
+
+  let htmlTemplate = "<h1>High Scores</h1>";
+  for (let player of getHighScoresBack) {
+    htmlTemplate += `<p>${player[0]} Score ${player[1]}</p>`;
   }
+  htmlTemplate += `<button class="goBackButton">Go Back</button>
+                  <button class="clearHighScoresButton">Clear High Scores</button>`;
+  quizContent.innerHTML = htmlTemplate;
+}
+
+function showNextQuestion() {
+  if (questionsQueue.length > 1) {
+    // Removes the first question from questionsQueue
+    questionsQueue.shift();
+    console.log(questionsQueue);
+    // Pass first element in line to first question
+    currentQuestion = questionsQueue[0];
+    setGameBoard();
+  } else {
+    stopTimer = true;
+    console.log("Game Over");
+    finalScore = timer;
+    quizContent.innerHTML = `<h1>Your Score is ${finalScore}</h1> Enter Your Name 
+            <label for="userName">Your Name</label><br>
+            <input type="text" id="userName" name="userName"><br>
+            <button class="submitName" type="submit">Submit</button>
+            `;
+  }
+}
+
+function checkAnswer(userInput) {
+  if (userInput == currentQuestion.correctAns) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function shuffleAnswers(anArray) {
+  for (let i = anArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    //Swap array i and array j
+    [anArray[i], anArray[j]] = [anArray[j], anArray[i]];
+  }
+  return anArray;
+}
+
+function setGameTimer() {
+  timer = 75;
+  let everySecond = setInterval(function () {
+    timer--;
+    timeArea.textContent = timer + " seconds left!";
+    if (timer === 0 || stopTimer == true) {
+      clearInterval(everySecond);
+      timeArea.textContent = "Time's Up";
+      timer = 0;
+      console.log(0);
+    }
+  }, 1000);
+}
+
+function hideButtons() {
+  instructions.style.display = "none";
+  viewHighScoreButton.style.display = "none";
+  startButton.style.display = "none";
+}
+
+viewHighScoreButton.addEventListener("click", function () {
+  displayAllUserHighScores();
+});
+
+function unhideButtons() {
+  startButton.style.display = "block";
+  viewHighScoreButton.style.display = "block";
+}
+
+function restartEverything() {
+  stopTimer = false;
+  finalScore = 0;
+  timer = 0;
+  questionsQueue = [];
+  currentQuestion = {};
+}
